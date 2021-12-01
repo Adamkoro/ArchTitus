db4727c6fb5678c94e2976c72d3e2873abd73737
adamkoro <akoronics@protonmail.com>
2021-12-01 14:51:43 +0100

    For running as sudo

--- flatpak-apps.sh
+++ flatpak-apps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install apps
-flatpak install -y flathub org.gtk.Gtk3theme.Breeze-Dark
-flatpak install -y flathub com.teamspeak.TeamSpeak
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub com.github.wwmm.pulseeffects
-flatpak install -y flathub com.spotify.Client
+sudo flatpak install -y flathub org.gtk.Gtk3theme.Breeze-Dark
+sudo flatpak install -y flathub com.teamspeak.TeamSpeak
+sudo flatpak install -y flathub com.discordapp.Discord
+sudo flatpak install -y flathub com.github.wwmm.pulseeffects
+sudo flatpak install -y flathub com.spotify.Client
 
 # Setting theme
-flatpak override --env=GTK_THEME=Breeze-Dark com.sindresorhus.Caprine
-flatpak override --env=GTK_THEME=Breeze-Dark com.github.wwmm.pulseeffects
+sudo flatpak override --env=GTK_THEME=Breeze-Dark com.sindresorhus.Caprine
+sudo flatpak override --env=GTK_THEME=Breeze-Dark com.github.wwmm.pulseeffects
