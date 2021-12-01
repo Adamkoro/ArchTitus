ef7133cab8690e8701d1264ace667e1a69e9fdab
adamkoro <akoronics@protonmail.com>
2021-12-01 16:29:26 +0100

    Fixed install dir

--- bashrc-install.sh
+++ bashrc-install.sh
@@ -6,6 +6,6 @@ if [ $(id -u) -eq 0 ];
 then
     cp root-bashrc /root/.bashrc
 else
-    cp user-bashrc /root/.bashrc
+    cp user-bashrc /home/${USER}/.bashrc
 fi
 echo "Shell changed and installed successfully"
