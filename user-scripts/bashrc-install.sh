270801bf1200b5fb41a5504f0693d7fc9c7aa875
adamkoro <akoronics@protonmail.com>
2021-12-07 10:46:13 +0100

    Git extension sh added for install script

--- user-scripts/bashrc-install.sh
+++ user-scripts/bashrc-install.sh
@@ -4,7 +4,9 @@ chsh -s /bin/bash
 
 if [ $(id -u) -eq 0 ]; then
     cp -r bash/.* /root/
+    cp /usr/share/git/git-prompt.sh /root/
 else
     cp -r bash/.* /home/${USER}/
+    cp /usr/share/git/git-prompt.sh /home/${USER}/
 fi
 echo "Shell setting installed and changed successfully"
