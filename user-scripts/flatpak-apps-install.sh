8365f4de8d984a6301c337caf339f93af0b667e4
adamkoro <akoronics@protonmail.com>
2021-12-08 09:26:51 +0100

    Unneeded apps commented out

--- user-scripts/flatpak-apps-install.sh
+++ user-scripts/flatpak-apps-install.sh
@@ -4,9 +4,9 @@
 sudo flatpak install -y flathub org.gtk.Gtk3theme.Breeze-Dark
 sudo flatpak install -y flathub com.teamspeak.TeamSpeak
 sudo flatpak install -y flathub com.discordapp.Discord
-sudo flatpak install -y flathub com.github.wwmm.pulseeffects
+#sudo flatpak install -y flathub com.github.wwmm.pulseeffects
 #sudo flatpak install -y flathub com.spotify.Client
 
 # Setting theme
 sudo flatpak override --env=GTK_THEME=Breeze-Dark com.sindresorhus.Caprine
-sudo flatpak override --env=GTK_THEME=Breeze-Dark com.github.wwmm.pulseeffects
+#sudo flatpak override --env=GTK_THEME=Breeze-Dark com.github.wwmm.pulseeffects
