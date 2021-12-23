228d89141484da9affb290ac08d255ad4f4a2a8c
adamkoro <akoronics@protonmail.com>
2021-12-23 16:30:49 +0100

    Previus error fixed(defautl gateway)

--- user-scripts/bridge-bond.sh
+++ user-scripts/bridge-bond.sh
@@ -8,11 +8,12 @@ BOND_SLAVE1=enp5s0
 BOND_MODE=5
 IP_ADDRESS=192.168.1.100/24
 DNS_SERVERS=1.1.1.1,1.0.0.1
-DEFAULT_ROUTE=192.168.1.0/24 192.168.1.254
+DEFAULT_ROUTE=192.168.1.0/24
+DEFAULT_GATEWAY=192.168.1.254
 
 nmcli con add ifname "${BRIDGE}" type bridge con-name "${BRIDGE}"
 nmcli con modify "${BRIDGE}" bridge.stp "${BRIDGE_STP}"
-nmcli con modify "${BRIDGE}" ipv4.method manual ipv4.address "${IP_ADDRESS}" ipv4.dns "${DNS_SERVERS}" ipv4.routes "${DEFAULT_ROUTE}" ipv6.method disable
+nmcli con modify "${BRIDGE}" ipv4.method manual ipv4.address "${IP_ADDRESS}" ipv4.dns "${DNS_SERVERS}" ipv4.routes "${DEFAULT_ROUTE}" ipv4.gateway "${DEFAULT_GATEWAY}" ipv6.method disable
 nmcli con up "${BRIDGE}"
 
 nmcli con add type bond ifname "${BOND}" con-name "${BOND}"
